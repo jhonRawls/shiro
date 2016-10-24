e7d37d1bf60096e80427ee884f47a7ac16db3456
jhon- <jhon-@DESKTOP-DKTFTUG>
2016-10-24 16:43:36 +0800

    提交配置文件

--- src/main/java/com/ibadi/shiro/App.java
+++ src/main/java/com/ibadi/shiro/App.java
@@ -1,13 +1,20 @@
 package com.ibadi.shiro;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+
+
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		
+	}
 }
--- src/test/java/com/ibadi/shiro/HelloWorldTest.java
+++ src/test/java/com/ibadi/shiro/HelloWorldTest.java
@@ -15,10 +15,13 @@ public class HelloWorldTest {
 	@SuppressWarnings("unused")
 	@Test
 	public void testHelloWorld() {
+		System.out.println("Hello World!");
 		// 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
-		Factory<org.apache.shiro.mgt.SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+		Factory<org.apache.shiro.mgt.SecurityManager> factory =
+		new IniSecurityManagerFactory("classpath:shiro.ini");
 		// 2、得到SecurityManager实例 并绑定给SecurityUtils
-		org.apache.shiro.mgt.SecurityManager securityManager = factory.getInstance();
+		org.apache.shiro.mgt.SecurityManager securityManager =factory.getInstance();
+		SecurityUtils.setSecurityManager(securityManager);
 		// 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
 		Subject subject = SecurityUtils.getSubject();
 		UsernamePasswordToken token = new UsernamePasswordToken("zhang", "123");
